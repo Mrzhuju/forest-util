23c278b0861f4844c159394fd7ac58cf78f33ca6
zhujusen <zhujusen@meizu.com>
2023-07-27 18:23:14 +0800

    Initial commit

---
+++ src/test/java/com/forest/util/ForestUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forest.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
